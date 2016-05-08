96c2cf15c608f4d5c088720b5e5651bdfd9ca5e0
gmasouras <masourasgeorge@gmail.com>
2016-05-08 21:21:32 +0200

    Reflections.txt added and some useful changes done as my reviewer suggested me

---
+++ app.rb
@@ -0,0 +1,4 @@
+require "colorizr"
+
+String.sample_colors
+puts "adfasd".pink
--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,8 +1,10 @@
 class String
 
+    @@my_colors = {red: 31, green: 32, yellow: 33, blue: 34, pink: "38;5;218", light_blue: 94, white: 97, light_grey: 37, black: 30}
+
 	def self.create_colors
-		my_colors = {red: 31, green: 32, yellow: 33, blue: 34, pink: "38;5;218", light_blue: 94, white: 97, light_grey: 37, black: 30}
-		my_colors.each {|key, value|
+		# my_colors = {red: 31, green: 32, yellow: 33, blue: 34, pink: "38;5;218", light_blue: 94, white: 97, light_grey: 37, black: 30}
+		@@my_colors.each {|key, value|
 			send(:define_method, "#{key}") {
 				"\e[#{value}m#{self}\e[0m"
 			}
@@ -10,7 +12,7 @@ class String
 	end
 
     def self.colors
-    	create_colors.map {|key, value| key}
+    	@@my_colors.map {|key, value| key}
     end
 
 
@@ -22,8 +24,8 @@ class String
     private 
 
     def self.print_colors
-    	my_colors = colors.map {|color| color.to_s}
-    	my_colors.each { |col|
+    	my_colors_to_s = colors.map {|color| color.to_s}
+    	my_colors_to_s.each { |col|
     		puts "This is "+"#{col}".send(col)
     	}
     end
